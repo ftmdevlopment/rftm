17717b98f2cc129b6bf1846e11f4dd4bc2db634f
Siwei Xu <xusiwei1236@163.com>
2016-12-14 16:55:57 +0800

    [test] add empty test

---
+++ jni/test/empty_test.h
@@ -0,0 +1,19 @@
+//
+// Created by dev on 16-12-14.
+//
+
+#ifndef RFTM_EMPTY_TEST_H
+#define RFTM_EMPTY_TEST_H
+
+#include "../ui_test.h"
+
+class EmptyTest : public UiTest
+{
+public:
+    UITEST_ENTRY(EmptyTest) {}
+
+private:
+};
+
+
+#endif //RFTM_EMPTY_TEST_H
